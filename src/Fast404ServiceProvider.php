26ca02ee1417a2b6f2e541c8adc19d260569529c
Ayesh Karunaratne <ayesh@ayesh.me>
2020-05-20 21:02:21 +0700

    Implement DeferrableProvider in the bundled service provider

--- src/Fast404ServiceProvider.php
+++ src/Fast404ServiceProvider.php
@@ -3,13 +3,18 @@
 namespace PHPWatch\LaravelFast404;
 
 use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class Fast404ServiceProvider extends ServiceProvider {
-    public function register() {
+class Fast404ServiceProvider extends ServiceProvider implements DeferrableProvider {
+    public function register(): void {
         $this->app->get(Kernel::class)->prependMiddleware(Fast404Middleware::class);
         $this->app->singleton(Fast404Middleware::class, static function ($app): Fast404Middleware {
             return new Fast404Middleware(config('app.fast404.message', 'Not Found'), config('app.fast404.regex', null), config('app.fast404.exclude_regex', null));
         });
+    }
+
+    public function provides(): array {
+        return [Fast404Middleware::class];
     }
 }
