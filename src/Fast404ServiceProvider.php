564b7916e7d60911b5b2c1bbe9cecf01eb09794d
Ayesh Karunaratne <ayesh@ayesh.me>
2020-05-20 23:13:16 +0700

    Cleanup service provider

--- src/Fast404ServiceProvider.php
+++ src/Fast404ServiceProvider.php
@@ -3,18 +3,16 @@
 namespace PHPWatch\LaravelFast404;
 
 use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class Fast404ServiceProvider extends ServiceProvider implements DeferrableProvider {
+class Fast404ServiceProvider extends ServiceProvider {
     public function register(): void {
-        $this->app->get(Kernel::class)->prependMiddleware(Fast404Middleware::class);
         $this->app->singleton(Fast404Middleware::class, static function ($app): Fast404Middleware {
             return new Fast404Middleware(config('app.fast404.message', 'Not Found'), config('app.fast404.regex', null), config('app.fast404.exclude_regex', null));
         });
     }
 
-    public function provides(): array {
-        return [Fast404Middleware::class];
+    public function boot(): void {
+        $this->app->get(Kernel::class)->prependMiddleware(Fast404Middleware::class);
     }
 }
