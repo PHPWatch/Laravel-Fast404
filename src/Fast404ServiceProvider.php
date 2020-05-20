566bae1f47f0936d7c471bac195c4f35afef9c3e
Ayesh Karunaratne <ayesh@ayesh.me>
2020-05-20 19:32:47 +0700

    Add Fast404ServiceProvider

---
+++ src/Fast404ServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPWatch\LaravelFast404;
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Support\ServiceProvider;
+
+class Fast404ServiceProvider extends ServiceProvider {
+    public function register() {
+        $this->app->get(Kernel::class)->prependMiddleware(Fast404Middleware::class);
+        $this->app->singleton(Fast404Middleware::class, static function ($app): Fast404Middleware {
+            return new Fast404Middleware(config('app.fast404.message', 'Not Found'), config('app.fast404.regex', null), config('app.fast404.exclude_regex', null));
+        });
+    }
+}
